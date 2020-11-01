b4f1aa55b142a3d09a2cbeb73ec9027cfb4a2352
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-01 14:45:20 -0800

    Complete Phase 2

--- w7/W7D1/rails-auth/rails_auth_app/app/models/user.rb
+++ w7/W7D1/rails-auth/rails_auth_app/app/models/user.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  session_token   :string           not null
+#
 class User < ApplicationRecord
+    before_validation :ensure_session_token
+    validates :username, :session_token, presense: true
+    validates :password_digest, presense: { message: "Password can't be blank"}
+    validates :password, length: { minimum: 6, allow_nil: true }
 end
