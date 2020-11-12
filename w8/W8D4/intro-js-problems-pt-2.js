0a491a553971adaebbce44d94dce0fa3d9325928
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-12 00:41:30 -0800

    EOD

--- w8/W8D4/intro-js-problems-pt-2.js
+++ w8/W8D4/intro-js-problems-pt-2.js
@@ -39,3 +39,16 @@ eleph.addTrick('juggle')
 eleph.addTrick('wink')
 eleph.play()
 
+let ellie = new Elephant("Ellie", 185, ["giving human friends a ride", "playing hide and seek"]);
+let charlie = new Elephant("Charlie", 200, ["painting pictures", "spraying water for a slip and slide"]);
+let kate = new Elephant("Kate", 234, ["writing letters", "stealing peanuts"]);
+let micah = new Elephant("Micah", 143, ["trotting", "playing tic tac toe", "doing elephant ballet"]);
+
+let herd = [ellie, charlie, kate, micah];
+
+Elephant.prototype.paradeHelper = function (elephant){
+    console.log(`${this.name} is trotting by!`)
+}
+
+console.log(herd.forEach(elephant => elephant.__proto__.paradeHelper))
+
