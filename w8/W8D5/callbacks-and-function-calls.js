6c50d055e5b08f6babc4e899d5b96699b37b2481
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-12 22:27:51 -0800

    Complete Timeout Plus Closure

--- w8/W8D5/callbacks-and-function-calls.js
+++ w8/W8D5/callbacks-and-function-calls.js
@@ -1,3 +1,9 @@
 //Simple Timeout
 window.setTimeout(function(){alert("HAMMERTIME")}, 5000);
 //Asynchronous
+
+//Timeout Plus Closure
+function hammerTime(time) {
+    window.setTimeout(function () { alert(`${time} is hammertime!`) }, 5000);
+}
+hammerTime();
