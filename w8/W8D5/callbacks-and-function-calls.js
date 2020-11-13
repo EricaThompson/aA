70d9e7bcb1342a2f786994ebff875af2a5e34679
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-13 08:42:01 -0800

    Add EOD

--- w8/W8D5/callbacks-and-function-calls.js
+++ w8/W8D5/callbacks-and-function-calls.js
@@ -1,3 +1,6 @@
+const { read } = require('fs');
+const readline = require('readline');
+
 //Simple Timeout
 window.setTimeout(function(){alert("HAMMERTIME")}, 5000);
 //Asynchronous
@@ -7,3 +10,19 @@ function hammerTime(time) {
     window.setTimeout(function () { alert(`${time} is hammertime!`) }, 5000);
 }
 hammerTime();
+
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+reader.question("Would you like some tea?", (response) => {
+    firstResponse = response;
+    console.log(`You replied ${response}`);
+});
+
+reader.question('Would you like some biscuits?', (response) => {
+    secondResponse = response;
+    console.log(`You replied ${response}.`);
+});
