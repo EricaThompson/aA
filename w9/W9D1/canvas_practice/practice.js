112d934bebf6b559662e54823996df6ecbd66206
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-14 12:37:40 -0800

    Complete Phase II

--- w9/W9D1/canvas_practice/practice.js
+++ w9/W9D1/canvas_practice/practice.js
@@ -3,3 +3,8 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 const canvas = document.getElementById('mycanvas');
 const ctx = canvas.getContext('2d');
+
+//fill color
+ctx.fillStyle = "turquoise"
+//fillRect(fromLeft, fromTop, width, height )
+ctx.fillRect(100, 100, 100, 50);
