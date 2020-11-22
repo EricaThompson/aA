10fdfcd8dfa85b00bbf02142d8da022845c02f26
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-21 16:06:43 -0800

    Add input to Calculator component

--- w10/W10D2/calculator/calculator.jsx
+++ w10/W10D2/calculator/calculator.jsx
@@ -10,11 +10,18 @@ class Calculator extends React.Component {
         }
     }
 
-    // your code here
+    setNum1(event){
+        this.setState({ num1: event.value })
+    }
+    setNum2(event) {
+        this.setState({ num2: event.value })
+    }
 
     render() {
         return (
             <div>
+                <input onChange={this.setNum1 = this.setNum1.bind(this)} type="text" value={this.state.num1}/>
+                <input onChange={this.setNum2 = this.setNum2.bind(this)} type="text" value={this.state.num2} />
                 <h1>{this.state.result}</h1>
             </div>
         );
