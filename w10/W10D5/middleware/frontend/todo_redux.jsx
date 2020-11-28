843e0ef38d3060634b28467a597f9a3b5167b6b4
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-27 22:54:49 -0800

    Complete phase 2

--- w10/W10D5/middleware/frontend/todo_redux.jsx
+++ w10/W10D5/middleware/frontend/todo_redux.jsx
@@ -4,11 +4,23 @@ import configureStore from './store/store';
 
 import Root from './components/root';
 
+
 document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
-    JSON.parse(localStorage.state) : {};
-  const store = configureStore(preloadedState);
-
+  JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+  store.dispatch = addLoggingToDispatch(store);
+  
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
+
+const addLoggingToDispatch = (store) => {
+  let newStore = store.dispatch;
+  return (action) => {
+    console.log(store.getState());
+    console.log(action);
+    newStore(action);
+    console.log(store.getState());
+  }
+}
