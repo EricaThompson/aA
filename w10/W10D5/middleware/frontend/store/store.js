b0398cb389b1f94b4a7e0fc201855516d3d3f248
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-28 00:04:55 -0800

    Complete Bonus Phase: More Middleware

--- w10/W10D5/middleware/frontend/store/store.js
+++ w10/W10D5/middleware/frontend/store/store.js
@@ -3,7 +3,8 @@ import rootReducer from '../reducers/root_reducer';
 import { applyMiddleware } from 'redux';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(rootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  const store = createStore(rootReducer, preloadedState, 
+    applyMiddleware(addLoggingToDispatch, secondMiddleware));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
@@ -14,7 +15,14 @@ const configureStore = (preloadedState = {}) => {
 const addLoggingToDispatch = store => next => action => {
   console.log(store.getState());
   console.log(action);
-  next(action);
+  console.log(next(action));
+  console.log(store.getState());
+}
+
+const secondMiddleware = store => next => action => {
+  console.log(store.getState());
+  console.log(action);
+  console.log(next(action));
   console.log(store.getState());
 }
 
