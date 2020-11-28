a1eba3e574ef3f4d76892c693278a9249df1524f
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-27 23:57:36 -0800

    Complete Phase 3: Redux applyMiddleware

--- w10/W10D5/middleware/frontend/todo_redux.jsx
+++ w10/W10D5/middleware/frontend/todo_redux.jsx
@@ -9,27 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const preloadedState = localStorage.state ?
   JSON.parse(localStorage.state) : {};
   let store = configureStore(preloadedState);
-  store = applyMiddlewares(store, addLoggingToDispatch);
+  // store = applyMiddlewares(store, addLoggingToDispatch);
   
   const root = document.getElementById('content');
   ReactDOM.render(<Root store={store} />, root);
 });
 
-const addLoggingToDispatch = store => next => action => {
-      // let newStore = store.dispatch;
-      console.log(store.getState());
-      console.log(action);
-      next(action);
-      console.log(store.getState());
 
-}
+// const applyMiddlewares = (store, ...middlewares) => {
+//   let dispatch = store.dispatch
 
-const applyMiddlewares = (store, ...middlewares) => {
-  let dispatch = store.dispatch
+//   middlewares.forEach(ware => {
+//     dispatch = ware(store)(dispatch)
+//   })
 
-  middlewares.forEach(ware => {
-    dispatch = ware(store)(dispatch)
-  })
-
-  return Object.assign({}, store, {dispatch});
-}
+//   return Object.assign({}, store, {dispatch});
+// }
