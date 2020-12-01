d4545c791e2c9b2f8e9d37c346fb97e713ae93bb
“ericathompson” <ericathompson.nc@gmail.com>
2020-11-30 23:35:32 -0800

    Add giphysReducer

--- w11/W11D1/giphy/reducers/giphys_reducer.js
+++ w11/W11D1/giphy/reducers/giphys_reducer.js
@@ -1,1 +1,15 @@
 import { RECEIVE_SEARCH_GIPHYS } from '../actions/giphy_actions';
+
+const giphysReducer = (state, action) => {
+    Object.freeze(state);
+    let nextState = Object.assign({}, state)
+
+    switch (action.type) {
+        case RECEIVE_SEARCH_GIPHYS:
+            nextState = action.giphys ;
+            return nextState;
+        default:
+            return state;
+    }
+
+}
