48cba0c2429a0868c031a95c5c49887bdf667a00
“ericathompson” <ericathompson.nc@gmail.com>
2020-12-02 00:08:20 -0800

    Complete Rainbow Routes

--- w11/W11D2/rainbow-routes/components/red.jsx
+++ w11/W11D2/rainbow-routes/components/red.jsx
@@ -9,9 +9,9 @@ class Red extends React.Component {
       <div>
         <h2 className="red"></h2>
         {/* Links here */}
-        <Link to='/red' >Red only</Link>
-        <Link to='/red/yellow' >Add yellow</Link>
-        <Link to='/red/orange' >Add orange</Link>
+        <NavLink exact to='/red' >Red only</NavLink>
+        <NavLink to='/red/yellow' >Add yellow</NavLink>
+        <NavLink to='/red/orange' >Add orange</NavLink>
         {/* Routes here */}
         <Route path="/red/orange" component={Orange} />
         <Route path="/red/yellow" component={Yellow} />
